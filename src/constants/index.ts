5ab0fab2511ba3a12bcd4241856b040f48682697
Nguyen Viet Hoang <84985941+viethoang01@users.noreply.github.com>
2025-06-21 18:25:11 +0700

    thêm file helper

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,3 +18,10 @@ export const LOANS_STEPS =  [
         },
     ] as STEP_TYPE[]
 
+function formatNumber(value: number | string) {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function parseFormattedNumber(value: string) {
+  return Number(value.replace(/\./g, ""));
+}
